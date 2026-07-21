62c2a5834492ae32eec91773190af76d19f9e066
FazriAhmad2004 <fazriahmad2004@gmail.com>
2026-07-21 15:38:19 +0530

    Update images 2  and image paths

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,11 @@ function About() {
 
           {/* Left Image */}
           <div>
-            <img
-src={`${import.meta.env.BASE_URL}images/test1.jpeg`}
-                alt="ACA Importers"
-              className="rounded-3xl shadow-2xl w-full h-[550px] object-cover"
-            />
+<img
+  src={`${import.meta.env.BASE_URL}images/test1.jpeg`}
+  alt="ACA Importers"
+  className="rounded-3xl shadow-2xl w-full h-[550px] object-cover"
+/>
           </div>
 
           {/* Right Content */}
